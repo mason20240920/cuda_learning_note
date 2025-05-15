84cab2049f7a326dab30fe6fab2bb43bbd27954f
mason20240920 <2420615378@qq.com>
2025-05-15 02:36:25 +0000

    first commit

---
+++ build/1_cuda_sgemm/CMakeFiles/origin_sgemm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/origin_sgemm.dir/origin_sgemm.cu.o"
+  "CMakeFiles/origin_sgemm.dir/origin_sgemm.cu.o.d"
+  "origin_sgemm"
+  "origin_sgemm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/origin_sgemm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
